cf27cbe1b933377ff6aaa0a305a533e3b9d212a4
jenish <sabhadiyajenish83@gmail.com>
2024-06-05 18:31:35 +0530

    desfe

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'ad0386f9b61bbc624f7d');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '1e46b4ce8889103ea180');
